b29e468dabef929e47b7424e04adf8e1cf8d1d20
Ha <nguyenhoangha1342001@gmail.com>
2022-06-25 11:10:04 +0700

    Update folder name

--- app/src/main/java/com/example/tiktokapp/presentation/SplashActivity.kt
+++ app/src/main/java/com/example/tiktokapp/presentation/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.example.tiktokapp
+package com.example.tiktokapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiktokapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
