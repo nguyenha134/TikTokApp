dabec0b16b7c9b8d553e99e5918e2c913130ceda
Ha <nguyenhoangha1342001@gmail.com>
2022-06-27 09:58:33 +0700

    Update Plash and Main activity

--- app/src/main/java/com/example/tiktokapp/presentation/SplashActivity.kt
+++ app/src/main/java/com/example/tiktokapp/presentation/SplashActivity.kt
@@ -1,12 +1,49 @@
 package com.example.tiktokapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.example.tiktokapp.R
+import com.example.tiktokapp.databinding.ActivityMainBinding
+import com.example.tiktokapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private val SPLASH_SCREEN = 3000
+
+    private lateinit var topAnimation : Animation
+    private lateinit var bottomAnimation : Animation
+
+    private lateinit var imgIconTikTok : ImageView
+    private lateinit var imgTikTok : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        binding.imgIconTikTok.animation = topAnimation
+
+        binding.imgTikTok.animation = bottomAnimation
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN.toLong())
+
     }
 }
